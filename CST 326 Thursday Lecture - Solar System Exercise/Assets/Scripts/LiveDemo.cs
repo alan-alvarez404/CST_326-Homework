cc4c425b29caf36e1dc676cad6cc760aad049465
Alan Alvarez <alvarezalanjose@gmail.com>
2026-01-24 15:05:50 -0800

    Commit - moved the solar system project to a different folder
    Still needs the rest of the planets however
    Also added a .gitignore since for some reason this project was missing one

---
+++ CST 326 Thursday Lecture - Solar System Exercise/Assets/Scripts/LiveDemo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LiveDemo : MonoBehaviour
+{
+    public float yawDegreesPerSecond = 45f;   
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform myTransform = GetComponent<Transform>();
+        myTransform.Rotate(new Vector3(0f, yawDegreesPerSecond * Time.deltaTime, 0f));
+        
+    }
+}
