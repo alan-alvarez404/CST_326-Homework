1e8a3ee18dd731c413ef9f78cc4f44b47759868c
Alan Alvarez <alvarezalanjose@gmail.com>
2026-01-24 17:27:44 -0800

    Commit - completed the displaying score and text tutorial
    Will now move on to the adding ai navigation tutorial
    Note: The size and position of the text from the ui compared to the tutorial is slightly different, but that's ok : )

--- Projects/CST 326 Project 0 - Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Projects/CST 326 Project 0 - Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -20,6 +28,16 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 11)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate()
@@ -34,6 +52,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
         }
     }
 }
