115f144860391192836c3408bbb51887ccfb4184
Alan Alvarez <alvarezalanjose@gmail.com>
2026-01-30 17:49:39 -0800

    Commit - did changes that were shown in the thursday lecture
    The ball falls but doesnt bounce
    But the paddle can be controlled
    Will work on making this a proper pong game

---
+++ Lectures/CST 326 - Thursday Lecture - Pong/Assets/Scripts/PongPaddle.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PongPaddle : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+    public float forcedStrength = 10f;
+    public float leftKey;
+    public float rightKey;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Vector3 force = new Vector3(0f, 0f, forcedStrength);
+            
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddForce(force, ForceMode.Force);
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Vector3 force = new Vector3(0f, 0f, -forcedStrength);
+            
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddForce(force, ForceMode.Force);
+        }
+
+        float angle = 50f;
+        
+        Vector3 up = Vector3.up;
+        Quaternion testRotation = Quaternion.Euler(60f, 0f, 0f);
+        
+        Vector3 rotatedVector = testRotation * up;
+
+        Quaternion otherRotation = Quaternion.Euler(-60f, 0f, 0f);
+        Vector3 otherRotatedVector = otherRotation * up;
+
+        Quaternion someOtherAngleRotation = Quaternion.Euler(angle, 0f, 0f);
+        Vector3 someOtherRotatedVector = someOtherAngleRotation * up;
+
+        Debug.DrawRay(transform.position, rotatedVector * 5f, Color.red);
+        Debug.DrawRay(transform.position, otherRotatedVector * 5f, Color.green);
+        Debug.DrawRay(transform.position, someOtherRotatedVector * 5f, Color.blue);
+    }
+}
