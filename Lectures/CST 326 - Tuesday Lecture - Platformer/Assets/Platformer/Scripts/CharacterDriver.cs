e36a3adaf9230fc8660ef1240f9e097032405449
Alan Alvarez <alvarezalanjose@gmail.com>
2026-02-20 17:55:26 -0800

    Commit - adjusted my script to be like the one in canvas
    This is so that I could follow yesterday's animation lecture better without any future hassles related to the movement script (hopefully).
    Also just moved what used to be the script in a txt folder in the same scripts folder in the Unity project.

--- Lectures/CST 326 - Tuesday Lecture - Platformer/Assets/Platformer/Scripts/CharacterDriver.cs
+++ Lectures/CST 326 - Tuesday Lecture - Platformer/Assets/Platformer/Scripts/CharacterDriver.cs
@@ -1,28 +1,30 @@
-using NUnit.Framework.Internal.Filters;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterDriver : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float runSpeed = 10f;
-    public float groundAcceleration = 15f;
+    public float groundAcceleration = 1f;
 
+    public float airAcceleration = 0.5f;
     public float apexHeight = 4.5f;
     public float apexTime = 0.5f;
     
-    Vector2 _velocity;
     CharacterController _controller;
-    Animator _animator;
+
+    float _velocityX;
+    float _velocityY;   
+    
     Quaternion facingLeft;
     Quaternion facingRight;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        facingRight = Quaternion.identity;
-        facingLeft = Quaternion.Euler(0f, 180f, 0f);
-        _animator = GetComponent<Animator>();
+        facingRight = Quaternion.Euler(0f, 90f, 0f);
+        facingLeft = Quaternion.Euler(0f, -90f, 0f);
         
         _controller =  GetComponent<CharacterController>();
     }
@@ -35,59 +37,51 @@ public class CharacterDriver : MonoBehaviour
         if (Keyboard.current.aKey.isPressed) direction -= 1f;
         bool jumpPressedThisFrame = Keyboard.current.spaceKey.wasPressedThisFrame;
         bool jumpHeld = Keyboard.current.spaceKey.isPressed;
-        
-        float gravityModifier = 1f;
+        bool runHeld = Keyboard.current.leftShiftKey.isPressed;
         
         if (_controller.isGrounded)
         {
             if (direction != 0)
             {
-                if (Mathf.Sign(direction) != Mathf.Sign(_velocity.x)) // Sign returns -1, +1, or 0
-                {
-                    _velocity.x = 0f;
-                }
-                _velocity.x += direction * groundAcceleration * Time.deltaTime;
-                _velocity.x = Mathf.Clamp(_velocity.x, -walkSpeed, walkSpeed);
-
+                _velocityX += direction * groundAcceleration * Time.deltaTime;
                 transform.rotation = (direction > 0f) ? facingRight : facingLeft;
             }
             else
             {
-                _velocity.x = Mathf.MoveTowards(_velocity.x, 0f, groundAcceleration * Time.deltaTime);
+                _velocityX = Mathf.MoveTowards(_velocityX, 0f, groundAcceleration * Time.deltaTime);
             }
 
             if (jumpPressedThisFrame)
             {
-                _velocity.y = 2f * apexHeight / apexTime;
-            }
-        }
-        else
-        {
-            if (!jumpHeld)
+                float jumpImpulse = 2f * apexHeight / apexTime;
+                _velocityY = jumpImpulse;
+            } else if (_velocityY < 0f)
             {
-                gravityModifier = 2f;
+                _velocityY = -1f; // Stay grounded
             }
         }
-
-        float gravity = 2f * apexHeight / (apexTime * apexTime);
-        _velocity.y -= gravity * gravityModifier * Time.deltaTime;
-
-        float deltaX = _velocity.x * Time.deltaTime;
-        float deltaY = _velocity.y * Time.deltaTime;
+        else // Air movement
+        {
+            float gravity = -2f * apexHeight / (apexTime * apexTime);
+            
+            if (!jumpHeld) gravity *= 2f;
+            _velocityY += gravity * Time.deltaTime;
+            _velocityX += direction * airAcceleration * Time.deltaTime;
+        }
         
-        Vector3 deltaPosition = new (deltaX, deltaY, 0f); // Don't have to say new Vector3 for some reason
+        // Speed clamping
+        float xMaxSpeed = runHeld ? runSpeed : walkSpeed;
+        _velocityX = Mathf.Clamp(_velocityX,-xMaxSpeed, xMaxSpeed);
+        
+        Vector3 deltaPosition = new Vector3(_velocityX, _velocityY, 0f) * Time.deltaTime;
+        
         CollisionFlags collisions = _controller.Move(deltaPosition);
         
-        if ((collisions & CollisionFlags.CollidedAbove) != 0)
-        {
-            _velocity.y = -1f;
-        }
-        if ((collisions & CollisionFlags.CollidedSides) != 0)
-        {
-            _velocity.x = 0f;
-        }
+        // Reset movement velocities based on object collisions
+        if ((collisions & CollisionFlags.Above) != 0 && _velocityY > 0f) _velocityY = 0f;
+        if ((collisions & CollisionFlags.Sides) != 0) _velocityX = 0f;
 
-        _animator.SetFloat("Speed", _velocity.x);
+        // _animator.SetFloat("Speed", _velocity.x);
 
         // Debug.Log($"Grounded: {_controller.isGrounded}"); // This runs every frame. Jesus Christ
     }
