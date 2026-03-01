f94a298f53f6b235570ea7892175a7eaa2a0d572
Alan Alvarez <alvarezalanjose@gmail.com>
2026-02-28 23:30:42 -0800

    Commit - added the score GUI that displays upon starting the project
    Will hide after 3 seconds or hide instantly once there's a left mouse button press.
    Also added the sprites needed for the enemies, the player, and other necessary things.
    Also imported a game looking font but will try to look for a different one.

--- Lectures/CST 326 - Tuesday Lecture - Schmup/Assets/2D Project/Scripts/GameManager.cs
+++ Lectures/CST 326 - Tuesday Lecture - Schmup/Assets/2D Project/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject scoreGUI;
+    public float scoreGUIDuration = 3f;
+    
     void Start()
     {
-       // todo - sign up for notification about enemy death 
+        if (scoreGUI != null)
+        {
+            scoreGUI.SetActive(true);
+        }
+
+        StartCoroutine(startingUI()); // Start the coroutine for the score GUI
+        
+       // Sign up for notification about enemy death 
        Enemy.OnEnemyDied += OnEnemyDied; // Suscribe
     }
 
@@ -16,5 +28,24 @@ public class GameManager : MonoBehaviour
     void OnEnemyDied(float score)
     {
         Debug.Log($"Killed enemy worth {score}");
+    }
+
+    // This handles the score GUI being displayed for 3 seconds as long as there's no lmb click
+    IEnumerator startingUI()
+    {
+        float timer = 0f; // Use this to count up
+        while (timer < scoreGUIDuration)
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                break; // Stop checking and go to disabling the GUI
+            }
+            
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Disable the score GUI
+        scoreGUI.SetActive(false);
     }
 }
